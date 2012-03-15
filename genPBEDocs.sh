f24d29ee0a1e171b933fa17b531dba1f2773683f
Nate Beck <nate@zaalabs.com>
2012-03-14 17:13:23 -0700

    Fixed the Jump To table of contents, thanks for the sanity check from Simeon Bateman

--- genPBEDocs.sh
+++
@@ -1,5 +0,0 @@
-date
-echo "Removing Previous Docs"
-rm -rf docs
-/projects/libraries/docco/bin/docco \
-    `find /projects/libraries/pbe2/src/* | grep \\.as | grep -v greensock` `find /projects/libraries/pbe2/examples/* | grep \\.as | grep -v greensock`
--- gendocs.sh
+++ gendocs.sh
@@ -2,4 +2,4 @@ date
 echo "Removing Previous Docs"
 rm -rf docs
 /projects/libraries/docco/bin/docco \
-    `find /projects/libraries/SmashFramework/src/* | grep \\.as | grep -v greensock` `find /projects/libraries/SmashFramework/examples/* | grep \\.as | grep -v greensock`
+    `find /projects/libraries/SmashFramework/src/ -name "*.as"` `find /projects/libraries/SmashFramework/examples/ -name "*.as" | grep -v greensock`
